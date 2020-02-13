de1da76f0e7a46562e1c1b446261592ee8346b93
marcelo.acelino <marcelo.acelino@LAB2-DCX-16.labs.dcx>
2020-02-13 08:23:38 -0300

    oi

---
+++ engenhariaSoftware/src/es/ufpb/Erivelton.java
@@ -0,0 +1,21 @@
+package es.ufpb;
+
+public class Erivelton {
+	
+static int a, b, resultado;
+	
+static int soma(int a, int b) {
+	return(a + b);
+
+}
+public static void main(String [] args) {
+
+	a = 5;
+	b = 5;
+	resultado = ( a+ b);
+	System.out.printf("resultado:", );
+
+	}
+
+}
+
