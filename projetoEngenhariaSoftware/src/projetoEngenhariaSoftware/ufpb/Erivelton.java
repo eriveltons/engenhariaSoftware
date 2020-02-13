1a19eb2de762bece6a8b8f7690f9768e87cde129
erivelton <erivelton@Erivelton-PC>
2020-02-13 19:29:51 -0300

    Boa noite!2

---
+++ projetoEngenhariaSoftware/src/projetoEngenhariaSoftware/ufpb/Erivelton.java
@@ -0,0 +1,19 @@
+package projetoEngenhariaSoftware.ufpb;
+
+public class Erivelton {
+
+	static int a, b, resultado;
+	
+	static int soma(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		a = 15;
+		b = 10;
+		resultado = (a+b);
+		System.out.println("Resultado: " +resultado);
+
+	}
+
+}
